3f24a2367fbfc86ab07369b694431530fe0c4253
Lucas Bonny <lucasbonnyb8@gmail.com>
2024-11-12 00:09:28 -0300

    Game repository, service, controller, DTO

---
+++ src/main/java/br/com/gunthercloud/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package br.com.gunthercloud.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gunthercloud.dslist.entities.dto.GameMinDTO;
+import br.com.gunthercloud.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll(){
+		List<GameMinDTO> list = gameService.findAll();
+		return list;
+	}
+
+}
